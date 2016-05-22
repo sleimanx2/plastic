243d531bc523a3a48f1303690532c6a67c3732ed
sleimanx2 <sleiman@outlook.com>
2016-05-22 13:32:39 +0300

    get method for map grammar

--- src/Connection.php
+++ src/Connection.php
@@ -4,23 +4,28 @@ namespace Sleimanx2\Plastic;
 
 use Elasticsearch\ClientBuilder;
 use Sleimanx2\Plastic\Map\Builder as MapBuilder;
+use Sleimanx2\Plastic\Map\Grammar as MapGrammar;
 
 class Connection
 {
 
     /**
+     * Elasticsearch client instance
+     *
      * @var
      */
     protected $elastic;
 
     /**
+     * Default elastic config
+     *
      * @var array
      */
     private $config = [
         'hosts'   => ['localhost:9200'],
-        'retries' => 2
+        'retries' => 2,
+        'index'   => 'default_index'
     ];
-
 
     /**
      * Connection constructor.
@@ -29,11 +34,12 @@ class Connection
      */
     public function __construct(array $config = [])
     {
-        $this->config = $config;
+        $this->config = empty($config) ? $this->config : $config;
 
-        $this->elastic = $this->initClient();
+        $this->elastic = $this->buildClient($this->config);
+
+        $this->index = $this->config['index'];
     }
-
 
     /**
      * Get map builder instance for this connection
@@ -46,15 +52,37 @@ class Connection
     }
 
     /**
+     * Get map grammar instance for this connection
+     *
+     * @return MapBuilder
+     */
+    public function getMapGrammar()
+    {
+        return new MapGrammar();
+    }
+
+    /**
+     * Execute a map statement on index;
+     *
+     * @param $mappings
+     * @return array
+     */
+    public function mapStatement($mappings)
+    {
+        return $this->elastic->indices()->putMapping(array_merge(['index' => $this->index], $mappings));
+    }
+
+    /**
      * Create an elastic search instance
      *
+     * @param array $config
      * @return \Elasticsearch\Client
      */
-    private function initClient()
+    private function buildClient(array $config)
     {
         return ClientBuilder::create()
-            ->setHosts($this->config['hosts'])
-            ->setRetries($this->config['retries'])
+            ->setHosts($config['hosts'])
+            ->setRetries($config['retries'])
             ->build();
     }
 }
