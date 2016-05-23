50413f0e013d2f35df48ad7448926f635738859f
sleimanx2 <sleiman@outlook.com>
2016-05-23 19:16:19 +0300

    added model listener for searchable trait

--- src/Searchable.php
+++ src/Searchable.php
@@ -5,15 +5,58 @@ namespace Sleimanx2\Plastic;
 
 trait Searchable
 {
+
+    public static function bootSearchable()
+    {
+        static::creating(function ($model) {
+
+            // fill the fields that should be mapped
+
+            // index the model
+
+        });
+
+        static::updating(function ($model) {
+
+            // fill the fields that should be mapped
+
+            // index the model
+
+            // collect related models that should be updated
+
+            // fill each related models with its new data
+
+            // bulk update related models
+
+        });
+
+        static::deleting(function ($model) {
+
+            // fill the fields that should be mapped
+
+            // index the model
+
+            // collect related models that should be updated
+
+            // fill each related models with its new data
+
+            // bulk update related models
+
+        });
+    }
+
     /**
-     * Return the elastic type used by the model
+     * Get the model elastic type
+     *
+     * @return string
      */
     public function getType()
     {
+        // if the type is defined use it else return the table name
         if (isset($this->type)) {
             return $this->type;
         }
 
-        $this->getTable();
+        return $this->getTable();
     }
 }
