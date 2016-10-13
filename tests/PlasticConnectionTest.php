81a8ea7190cd88a35ec170d55d985ac9a20adb1f
Sleiman Sleiman <sleiman@outlook.com>
2016-10-13 07:36:39 +0000

    Applied fixes from StyleCI

--- tests/PlasticConnectionTest.php
+++ tests/PlasticConnectionTest.php
@@ -19,9 +19,9 @@ class PlasticConnectionTest extends \PHPUnit_Framework_TestCase
     {
         $connection = $this->getConnectionMock();
 
-        $connection->setIndex("custom-index");
+        $connection->setIndex('custom-index');
 
-        $this->assertEquals("custom-index", $connection->index);
+        $this->assertEquals('custom-index', $connection->index);
     }
 
     /**
